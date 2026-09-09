7077ef62701f7c0a5ad58e68159c56d6b46ec225
Umar Mohammed <umarmoh2050@gmail.com>
2026-09-09 17:27:25 -0500

    Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Orion
 Orion is an AI personal assistant made by evolyx.
+Here is the stack:
+    - openWakeWord - Monitors for "Hey Orion" to be said to make the model start listening.
+    - faster-whisper - Speech to text; sends speech to Groq to be processed.
+    - Groq - Takes text from faster-whisper and processes it with an AI model, generating a response to the question and sending it back with API calls.
+             Groq then takes the output and processes it into an audio file, which is downloaded by the client and played.
--- src/main.py
+++ src/main.py
@@ -9,12 +9,6 @@
 | ╚══════╝  ╚═══╝   ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝ |
 ╰--------------------- Orion ----------------------╯
 
-Orion is an AI personal assistant made by evolyx.
-Here is the stack:
-    - openWakeWord - Monitors for "Orion" to be said to make the model start listening.
-    - faster-whisper - Speech to text; sending speech to the model to be processed.
-    - Groq - Takes text from faster-whisper and processes it with an AI model, generating a response to the question and sending it back with API calls.
-    - Piper - Text to speech; taking the result from Groq and turning it into audio.
-
+Main program sequence.
 """
 import wake_word
