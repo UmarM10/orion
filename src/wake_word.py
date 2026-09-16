a30c40f9835581897f3dba8abde8ddbb95a7d400
Umar Mohammed <umarmoh2050@gmail.com>
2026-09-15 21:09:36 -0500

    Updarted wake word module architecture.

--- src/wake_word.py
+++ src/wake_word.py
@@ -22,14 +22,18 @@ warnings.filterwarnings("ignore", category=UserWarning, module="onnxruntime")
 # "Hey Orion" custom trained model - ./hey_orion.tflite
 model = Model(wakeword_model_paths=[str(Path(__file__).parent / "models" / "hey_orion.onnx")])
 
-p = pyaudio.PyAudio()
-audio_stream = p.open(rate=16000, channels=1, format=pyaudio.paInt16, input=True)
-audio_stream.start_stream()
-print("Opened microphone stream: Listening for 'Hey Orion' above 50% confidence.")
 
-# Test script - runs only if executed directly.
-if __name__ == "__main__":
-    # On successful trigger, print "TRIGGERED" with a timestamp.
+
+def listen(debug=False):
+    # Blocking function
+    # On successful trigger, return confidence percentage as a float.
+
+    p = pyaudio.PyAudio()
+    audio_stream = p.open(rate=16000, channels=1, format=pyaudio.paInt16, input=True)
+    audio_stream.start_stream()
+    if debug: print("Audio input stream opened.")
+
+    prediction = None
     while True:
         try:
             raw_frame = audio_stream.read(num_frames=1280)
@@ -37,25 +41,30 @@ if __name__ == "__main__":
             prediction = model.predict(frame)
             if prediction["hey_orion"] >= numpy.float32(0.5):
                 model.reset()
-                print(f"TRIGGERED - {round(Decimal(str(prediction["hey_orion"])), 2) * 100}% confidence.")
+                if debug: print(f"Wake word triggered with {round(Decimal(str(prediction["hey_orion"])), 2)}% confidence.")
+                break
         except KeyboardInterrupt:
             print(" Interrupted: Exiting...")
-            break
+            audio_stream.stop_stream()
+            audio_stream.close()
+            return None
 
     audio_stream.stop_stream()
     audio_stream.close()
+    if debug: print("Audio input stream closed.")
+    if prediction: return round(Decimal(str(prediction["hey_orion"])), 2) * 100
 
-def listen():
-    # Blocking function
-    # On successful trigger, return confidence percentage as a float.
+# Test script - runs only if executed directly.
+if __name__ == "__main__":
+    print("Opened microphone stream: Listening for 'Hey Orion' above 50% confidence.")
+
+    print("Listening for wake word...")
     while True:
-        try:
-            raw_frame = audio_stream.read(num_frames=1280)
-            frame = numpy.frombuffer(raw_frame, dtype=numpy.int16)
-            prediction = model.predict(frame)
-            if prediction["hey_orion"] >= numpy.float32(0.5):
-                model.reset()
-                return round(Decimal(str(prediction["hey_orion"])), 2) * 100
-        except KeyboardInterrupt:
-            return
+        if listen(debug=True):
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print(" Interrupted: Exiting...")
+                break
+        else:
             break
